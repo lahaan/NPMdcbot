955b94017dd72ee71be57e9d347c6707128eb092
egg <ekemartinpaas@gmail.com>
2023-09-06 10:57:22 +0200

    changes

--- internals.py
+++ internals.py
@@ -2,10 +2,14 @@ import discord
 import responses
 
 
-async def send_message(message, user_message):
+async def send_message(username, message, user_message):
     try:
         response = responses.get_response(user_message)
-        await message.channel.send(response)
+        if user_message == 'star' and response[:1] == 'h':
+            await message.channel.send(f'{username}, you are such a nice guy (over 95% to be exact)')
+            await message.channel.send(response)
+        else:
+            await message.channel.send(response)
 
     except Exception as e:
         print(e)
@@ -30,10 +34,13 @@ def run_discord_bot():
         user_message = str(message.content)
         channel = str(message.channel)
 
-        print(f'{username} said: "{user_message}" ({channel}')
+        print(f'{username} said: "{user_message}" ({channel})')
+
+        if user_message.lower() in ['choice1', 'choice2']:
+            await send_message(username, message, 'kn')
 
         if user_message[0] == '!':
-            user_message = user_message[1:]
-            await send_message(message, user_message)
+            user_message = user_message[1:].lower()
+            await send_message(username, message, user_message)
 
     client.run(TOKEN)
--- responses.py
+++ responses.py
@@ -1,24 +1,59 @@
 import random
 
 
-def get_response(message: str) -> str:
+def get_response(message: str, ) -> str:
     p_message = message.lower()
 
     if message == 'nice':
-        meter = random.randint(0, 100)
         return poc_meter()
 
     if message == 'cock':
         character = '='
         return cock_meter()
 
+    if message == 'help':
+        return help_meter()
+
+    if message == 'star':
+        return star_meter()
+
+    if message == 'kn':
+        return kn()
+
+
 
 def cock_meter():
     character = '='
-    return f'8{random.randint(1, 10)*character}D'
+    return f'8{random.randint(0, 14)*character}D'
 
 
 def poc_meter():
     meter = random.randint(0, 100)
-    return f'You are {str(meter)}% black, good enough :thumbsup_tone1:' if meter < 50 else\
-        f'You are {str(meter)}% black,  :thumbsdown_tone1:'
+    if meter < 10:
+        return f'You are only {str(meter)}%  nice:smiling_face_with_3_hearts:'
+    return f'You are {str(meter)}% nice, unacceptable :thumbsdown_tone1:'if meter < 50 else\
+        f'You are a whopping {str(meter)}% , nice:'
+
+
+def help_meter():
+    return ('```Current commands: \n !nice [] \n !cock [measures your cock length] \n'
+            ' !star []```')
+
+
+def star_meter():
+    meter = random.randint(0,100)
+    if meter < 25:
+        return f'You are {meter}% star :thumbsup_tone1:'
+    elif 24 < meter < 50:
+        return f'You are {meter}% star :rolling_eyes:'
+    elif 49 < meter < 95:
+        return f'You are {meter}% star :star_of_david:'
+    else:
+        return ('https://media.discordapp.net/attachments/637444523861082132/'
+                '965640415845941268/jew_on_radar.gif?width=160&height=160')
+
+
+def kn():
+    response_list = ['choice1', 'choice2']
+    choice = random.randint(0, 7)
+    return response_list[choice]
