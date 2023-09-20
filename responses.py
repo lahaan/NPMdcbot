d3570c03b0d9e92d27329091f1ed70d4eaf30035
egg <ekemartinpaas@gmail.com>
2023-09-20 22:16:12 +0200

    0.1.2

--- responses.py
+++ responses.py
@@ -5,32 +5,32 @@ import data
 def get_response(message: str) -> str:
     meter = int(random.randint(0, 100))
 
-    if str(message) in data.n_call_list:
+    if message in data.n_call_list:
         return poc_meter(meter)
 
-    if str(message) in data.c_call_list:
-        return pp_meter()
+    if message in data.l_call_list:
+        return life_generator()
 
-    if str(message) in data.j_call_list:
-        return j_meter(meter)
-
-    if str(message) in data.f_call_list:
-        return f_meter(meter)
-
-    if str(message) in data.ns_call_list:
-        return slander()
-
-    if str(message) in data.pc_call_list:
+    if message in data.pc_call_list:
         return pc_generator()
 
-    if str(message) in data.i_call_list:
+    if message in data.c_call_list:
+        return pp_meter()
+
+    if message in data.j_call_list:
+        return j_meter(meter)
+
+    if message in data.f_call_list:
+        return f_meter(meter)
+
+    if message in data.ns_call_list:
+        return slander()
+
+    if message in data.i_call_list:
         return i_meter(meter)
 
-    if str(message) in data.t_call_list:
+    if message in data.t_call_list:
         return t_meter(meter)
-
-    if str(message) in data.l_call_list:
-        return life_generator()
 
     if message == 'help':
         return data.help_print
@@ -51,19 +51,15 @@ def get_response(message: str) -> str:
         return s_meter(meter)
 
     if message == 'version':
-        return '```Version 0.1.1 [15.09.23]\n  New: 3 more commands including !life/lifestory```'
+        return '```Version 0.1.2 [20.09.23]\n  New: Internal optimizations & expanded dataset```'
 
 
 def pp_meter():
-    meter = random.randint(1, 1000)
-    character = '='
-    if meter == 1000:
-        return '# 8================D'
-    return f'8{random.randint(0, 14)*character}D'
+    return f'8{random.randint(0, 14)*"="}D' if random.randint(1, 1000) != 1000 else '# 8================D'
 
 
 def poc_meter(meter):
-    if meter < 10:
+    if meter < 15:
         return f'You are only {meter}% {data.choices[2]} :smiling_face_with_3_hearts:'
     return f'You are {meter}% {data.choices[2]}, unacceptable :thumbsdown_tone1:'if meter < 50 else\
         f'You are a whopping {meter}% {data.choices[2]}, {data.choices[0]} {data.choices[3]}'
@@ -72,9 +68,9 @@ def poc_meter(meter):
 def j_meter(meter):
     if meter < 25:
         return f'You are {meter}% {data.choices[4]} :thumbsup_tone1:'
-    elif 24 < meter < 50:
+    elif meter < 50:
         return f'You are {meter}% {data.choices[4]} :rolling_eyes:'
-    elif 49 < meter < 95:
+    elif meter < 95:
         return f'You are {meter}% {data.choices[5]} :star_of_david:'
     else:
         return data.gif1
@@ -87,9 +83,9 @@ def kn():
 def c_meter(meter):
     if meter < 25:
         return f'You are {meter}% {data.choices[6]} :call_me:'
-    elif 24 < meter < 50:
+    elif meter < 50:
         return f'You are {meter}% {data.choices[6]} :nerd:'
-    elif 49 < meter < 95:
+    elif meter < 95:
         return f'You are {meter}% {data.choices[6]} :expressionless:'
     else:
         return f'You are {meter}% {data.choices[6]} {data.random_copypasta[0]}'
@@ -98,9 +94,9 @@ def c_meter(meter):
 def f_meter(meter):
     if meter < 25:
         return f'You are only {meter}% {data.choices[9]} :sunglasses:'
-    elif 24 < meter < 50:
+    elif meter < 50:
         return f'You are {meter}% {data.choices[9]} :flushed:'
-    elif 49 < meter < 95:
+    elif meter < 95:
         return f'You are {meter}% {data.choices[9]}, {data.random_copypasta[1]}'
     else:
         return f'You are {meter}% {data.choices[9]} - {data.random_copypasta[2]}'
@@ -155,9 +151,9 @@ def dram_cache():
 def s_meter(meter):
     if meter < 25:
         return f'You are {meter}% {data.choices[10]}, based'
-    elif 24 < meter < 50:
+    elif meter < 50:
         return f'You are {meter}% {data.choices[10]}, {data.random_copypasta[4]}'
-    elif 49 < meter < 95:
+    elif meter < 95:
         return f'You are {meter}% {data.choices[10]}, {data.random_copypasta[5]}'
     else:
         return f'You are {meter}% {data.choices[10]}, {data.random_copypasta[6]}'
@@ -166,9 +162,9 @@ def s_meter(meter):
 def i_meter(meter):
     if meter < 25:
         return f'You are {meter}% {data.choices[11]}, welcome!'
-    elif 24 < meter < 50:
+    elif meter < 50:
         return f'You are {meter}% {data.choices[11]}, {data.random_copypasta[7]}'
-    elif 49 < meter < 95:
+    elif meter < 95:
         return f'You are {meter}% {data.choices[11]}, {data.random_copypasta[8]}'
     else:
         return f'You are {meter}% {data.choices[11]}, {data.random_copypasta[9]}'
@@ -177,9 +173,9 @@ def i_meter(meter):
 def t_meter(meter):
     if meter < 25:
         return f'You are {meter}% {data.choices[12]}, {data.random_copypasta[10]}'
-    elif 24 < meter < 50:
+    elif meter < 50:
         return f'You are {meter}% {data.choices[12]}, {data.random_copypasta[11]}'
-    elif 49 < meter < 95:
+    elif meter < 95:
         return f'You are {meter}% {data.choices[12]}, {data.random_copypasta[12]}'
     else:
         return f'You are {meter}% {data.choices[12]}, {data.random_copypasta[13]}'
@@ -187,20 +183,27 @@ def t_meter(meter):
 
 def life_generator():
     gender_index = random.randint(0, 1)
-    age = random.randint(1, 50) if random.randint(1, 3) < 3 else random.randint(50, 119)
-    iq_score = str(random.randint(60, 140) if random.randint(1, 10) <= 9 else random.randint(140, 200))
+    age = age_generator()
+
+    iq_score = str(random.randint(60, 120) if random.randint(1, 20) < 20 else random.randint(120, 180))
+
     name = data.male_names[random.randint(0, data.male_names_amount)] if gender_index == 0 else\
         data.female_names[random.randint(0, data.female_names_amount)]
+
     gender = data.gender(gender_index)
     country = data.countries_list[random.randint(0, data.countries_list_amount)]
     clr = data.clr_list[random.randint(0, data.clr_list_amount)]
     salary = str(salary_generator())
     job = 'Jobless' if salary == '0' else data.job_titles[random.randint(0, data.job_titles_amount)]
+
     debt = str(random.randint(1, 100) * random.randint(1, 100) * random.randint(1, 10))\
         if random.randint(1, 3) == 1 else '0'
+
     whom_in_debt = data.whom_in_debt[random.randint(0, data.whom_in_debt_amount)] if debt != '0' else 'no one'
+
     age_of_death = str(random.randint(age + 1, 121))
     cause_of_death = data.cause_of_death[random.randint(0, data.cause_of_death_amount)]
+
     return f"```Here's your character: \n Name: {name} ({gender}, {age} y/o, IQ: {iq_score}) \n Race: {clr}"\
         f" \n Country of residence: {country} \n Job: {job} \n Salary (monthly): {salary}$ \n"\
         f" Debt: {debt}$ to {whom_in_debt} \n Age of death: {age_of_death} \n Cause of death: {cause_of_death}```"
@@ -208,28 +211,32 @@ def life_generator():
 
 def salary_generator():
     money_seed = random.randint(1, 1000)
-    final_salary = int
+    final_salary = 0
+
+    salary_ranges = {
+        50: (10, 100),       # 5%
+        200: (100, 500),     # 15%
+        400: (500, 1000),    # 20%
+        650: (1000, 2500),   # 25%
+        850: (2500, 5000),   # 20%
+        925: (1e3, 1e4),     # 7.5%
+        950: (1e4, 1e5),     # 2.5%
+        975: (1e5, 1e6),     # 2.5%
+        990: (1e7, 5e7),     # 1.5%
+        1000: (1e8, 1e9)     # 1%
+    }
+
     if random.randint(1, 10) != 1:
-        if money_seed < 50:
-            final_salary = 1000 * random.randint(1, 10) / 100    # ranges from 10-100 (5%)
-        if 49 < money_seed < 201:
-            final_salary = 1000 * random.randint(10, 50) / 100   # ranges from 100-500 (15%)
-        if 200 < money_seed < 401:
-            final_salary = 1000 * random.randint(50, 100) / 100  # ranges from 500-1000 (20%)
-        if 400 < money_seed < 651:
-            final_salary = 1000 * random.randint(10, 25) / 10    # ranges from 1000-2500 (25%)
-        if 650 < money_seed < 851:
-            final_salary = 1000 * random.randint(25, 50) / 10    # ranges from 2500-5000 (20%)
-        if 850 < money_seed < 926:
-            final_salary = 1000 * random.randint(50, 100) / 10   # ranges from 5000-10000 (7.5%)
-        if 925 < money_seed < 951:
-            final_salary = 1000 * random.randint(10, 100)        # ranges from 10000-100000 (2.5%)
-        if 950 < money_seed < 976:
-            final_salary = 100000 * random.randint(1, 100)       # ranges from 100000-10000000 (2.5%)
-        if 975 < money_seed < 990:
-            final_salary = 100000 * random.randint(100, 500)     # ranges from 10000000-50000000 (1.5%)
-        if money_seed > 990:
-            final_salary = 1000000000 * random.randint(1, 100) / 10  # idk anymore (0.1%)
+        for seed, salary_range in salary_ranges.items():
+            if money_seed <= seed:
+                final_salary = random.randint(*salary_range)
+                break
+
+    return final_salary
+
+
+def age_generator():
+    if random.randint(1, 3) == 1:
+        return random.randint(1, 15) if random.randint(1, 5) < 5 else random.randint(81, 119)
     else:
-        final_salary = 0
-    return int(final_salary)
+        return random.randint(16, 80)
